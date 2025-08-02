009e4a77d159fe245c754a9a9fba27fdd565c7d1
Gideon Mensah <mrgmens@gmail.com>
2025-08-02 05:08:50 +0000

    script that prints the full description of the table books from the database alx_book_store

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+-- Switch to the database
+USE alx_book_store;
+
+-- Retrieve full description of the 'Books' table
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
